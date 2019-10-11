e7422ca8700b8c10a81941fd0a32b58ce45305dd
Amir Arfan <amirarfan7@gmail.com>
2019-10-11 08:44:35 +0200

    Added board function

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -7,22 +7,24 @@ import random
 import time
 from matplotlib import pyplot as plt
 
-snakes_and_laders={
-    1:40,
-    8:10,
-    36:52,
-    43:62,
-    49:79,
-    65:82,
-    68:85,
-    24:5,
-    33:3,
-    42:30,
-    56:37,
-    64:27,
-    74:12,
-    87:70
-}
+def board():
+    snakes_and_laders={
+        1:40,
+        8:10,
+        36:52,
+        43:62,
+        49:79,
+        65:82,
+        68:85,
+        24:5,
+        33:3,
+        42:30,
+        56:37,
+        64:27,
+        74:12,
+        87:70
+    }
+    return snakes_and_laders
 
 def single_game(num_players):
     """
