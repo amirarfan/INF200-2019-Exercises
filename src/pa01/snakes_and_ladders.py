654dd464842dfb8f92e8272f7832a2307c8bb377
Sebastian Becker <sebabeck@nmbu.no>
2019-10-11 08:41:52 +0200

    snakes_and_laders dict

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -7,6 +7,23 @@ import random
 import time
 from matplotlib import pyplot as plt
 
+snakes_and_laders={
+    1:40,
+    8:10,
+    36:52,
+    43:62,
+    49:79,
+    65:82,
+    68:85,
+    24:5,
+    33:3,
+    42:30,
+    56:37,
+    64:27,
+    74:12,
+    87:70
+}
+
 def single_game(num_players):
     """
     Returns duration of single game.
