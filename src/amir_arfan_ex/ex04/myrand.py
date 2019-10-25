efe1ba2ef37c712b7daca371be4bc5d6928c3a3c
Amir Arfan <amirarfan7@gmail.com>
2019-10-25 13:09:37 +0200

    Finished all the functions, and ran test, test passed. Added docstrings, checked for PEP8

--- src/amir_arfan_ex/ex04/myrand.py
+++ src/amir_arfan_ex/ex04/myrand.py
@@ -6,22 +6,34 @@ __email__ = 'amar@nmbu.no'
 
 class LCGRand:
     def __init__(self, seed):
+        """
+        __init__ function, that takes only seed as an argument
+        """
         self.before = seed
-        self.a = 7 ** 5
-        self.m = 2 ** 31 - 1
+        self.a = 7 ** 5  # Defining a
+        self.m = 2 ** 31 - 1  # Defining m
 
     def rand(self):
-        self.before = (self.a * self.before) % self.m
+        self.before = (self.a * self.before) % self.m  # Generating the random
+        # Number
         return self.before
 
 
 class ListRand:
     def __init__(self, number_list):
+        """
+        Defining an init function that takes a number list (Already randomized)
+        as input
+        """
 
         self.random_number_list = number_list.copy()
         self.current = 0
 
     def rand(self):
+        """
+        Rand function that uses try, except to test if the user is asking
+        for a number which exceeds the list index.
+        """
         try:
             random_number = self.random_number_list[self.current]
         except IndexError:
--- src/amir_arfan_ex/ex04/walker.py
+++ src/amir_arfan_ex/ex04/walker.py
@@ -8,28 +8,64 @@ import random
 
 class Walker:
     def __init__(self, start_position, home_position):
+        """
+
+        Init function that takes in two arguments that are to be used
+        throughout the class.
+
+        """
         self.current_position = start_position
         self.final_position = home_position
         self.current_steps = 0
 
     def move(self):
+        """
+        Simulates a move, using python's integrated random function
+
+        """
         self.current_position += random.choice([-1, 1])
         self.current_steps += 1
 
     def is_at_home(self):
+        """
+        Checks if the walker is at home by using a bool test
+
+        """
         return self.current_position == self.final_position
 
     def get_position(self):
+        """
+        Simple function that returns the current position
+
+        """
         return self.current_position
 
     def get_steps(self):
+        """
+        Simple function that returns the current amount of steps taken.
+        """
         return self.current_steps
 
 
 def walker_start_home(start_position, home_position):
+    """
+    Walker function that simulates a walk through using
+    the 'Walker class'
+
+    """
     walker = Walker(start_position, home_position)
 
     while not walker.is_at_home():
         walker.move()
 
     return walker.get_steps()
+
+
+if __name__ == "__main__":
+    random.seed(100) # Allocating a seed for test purposes
+    num_sim = 5
+    dist = [2, 5, 10, 20, 50, 100]
+    for distance in dist:
+        result_list = [walker_start_home(0, distance) for simulations
+                       in range(num_sim)]
+        print(f'Distance: {distance} --> Path: {result_list}')
