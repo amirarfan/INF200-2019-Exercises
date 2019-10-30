0bedb763656628e8bd8b8fec1b5bc0a3f18460c8
Amir Arfan <amirarfan7@gmail.com>
2019-10-30 18:29:33 +0100

    Finished bounded sim, made it look prettier, attempted to use the super method as much as possible.

--- src/amir_arfan_ex/ex05/bounded_sim.py
+++ src/amir_arfan_ex/ex05/bounded_sim.py
@@ -23,95 +23,106 @@ class BoundedWalker(Walker):
             The right boundary  of walker movement
         """
 
-        if not left_limit <= start <= right_limit:
+        if not (left_limit <= start <= right_limit):
             raise RuntimeError('Your limits are not correct')
-        if not left_limit <= home <= right_limit:
+        elif not (left_limit <= home <= right_limit):
             raise RuntimeError('Your limits are not correct')
+
+        super().__init__(start, home)
+
+        # Using the subclass method as advised to do
 
         self.left_limit = left_limit
         self.right_limit = right_limit
 
-        super().__init__(start,
-                         home)  # Using the super method as advised to do
-
-    def move_walker(self):
+    def move(self):
         """
         Moves the walker with if tests such that the walker cannot move past
         limit
 
         """
         super().move()  # I am using the move function from the "Walker" Class
-        if self.current_position > self.right_limit:
-            self.current_position = self.right_limit
-        elif self.current_position < self.left_limit:
-            # using elif because
-            # one statement needs to checked
+        if self.current_position < self.left_limit:
             self.current_position = self.left_limit
+        elif self.current_position > self.right_limit:
+            self.current_position = self.right_limit
 
-    class BoundedSimulation(Simulation):
-        def __init__(self, start, home, seed, left_limit, right_limit):
-            """
-            Initialise the simulation
 
-            Arguments
-            ---------
-            start : int
-                The walker's initial position
-            home : int
-                The walk ends when the walker reaches home
-            seed : int
-                Random generator seed
-            left_limit : int
-                The left boundary of walker movement
-            right_limit : int
-                The right boundary  of walker movement
-            """
-            self.left_limit = left_limit
-            self.right_limit = right_limit
-            super().__init__(start, home, seed)  # Using subclass method
+class BoundedSimulation(Simulation):
+    def __init__(self, start, home, seed, left_limit, right_limit):
+        """
+        Initialise the simulation
 
-        def single_bounded_walk(self):
-            """
-            Simulate single walk from start to home, returning number of steps.
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        seed : int
+            Random generator seed
+        left_limit : int
+            The left boundary of walker movement
+        right_limit : int
+            The right boundary  of walker movement
+        """
 
-            Returns
-            -------
-            int
-                The number of steps taken
-            """
-            single_bounded_walker = BoundedWalker(self.start, self.home,
-                                          self.left_limit,
-                                          self.right_limit)
+        if not (left_limit <= start <= right_limit):
+            raise RuntimeError('Your limits are not correct')
+        elif not (left_limit <= home <= right_limit):
+            raise RuntimeError('Your limits are not correct')
 
-            while not single_bounded_walker.is_at_home():
-                single_bounded_walker.move()
+        # Added the same if test here as well, such that when you create the
+        # simulation class you will already know if your limits are wrong
 
-            return single_bounded_walker.get_steps()
+        super().__init__(start, home, seed)  # Using subclass method
+        self.left_limit = left_limit
+        self.right_limit = right_limit
 
-        def run_bounded_simulation(self, num_walks):
-            """
-            Run a set of walks, returns list of number of steps taken.
+    def single_bounded_walk(self):
+        """
+        Simulate single walk from start to home, returning number of steps.
 
-            Arguments
-            ---------
-            num_walks : int
-                The number of walks to simulate
+        Returns
+        -------
+        int
+            The number of steps taken
+        """
+        single_bounded_walker = BoundedWalker(self.start, self.home,
+                                              self.left_limit,
+                                              self.right_limit)
 
-            Returns
-            -------
-            list[int]
-                List with the number of steps per walk
-            """
+        while not single_bounded_walker.is_at_home():
+            single_bounded_walker.move()
 
-            list_walks = [self.single_bounded_walk() for _ in range(num_walks)]
+        return single_bounded_walker.get_steps()
 
-            return list_walks
+    def run_bounded_simulation(self, num_walks):
+        """
+        Run a set of walks, returns list of number of steps taken.
+
+        Arguments
+        ---------
+        num_walks : int
+            The number of walks to simulate
+
+        Returns
+        -------
+        list[int]
+            List with the number of steps per walk
+        """
+
+        list_walks = [self.single_bounded_walk() for _ in range(num_walks)]
+
+        return list_walks
 
 
 if __name__ == '__main__':
-    start = 0
-    home = 20
-    rb = 20
     boundaries = [0, -10, -100, -1000, -10000]
-
+    rb = 20
+    start_pos = 0
+    end_pos = 20
     for lb in boundaries:
+        current_sim = BoundedSimulation(start_pos, end_pos, 69, lb, rb)
+        steps = current_sim.run_bounded_simulation(20)
+        print(f'With left-boundary {lb}, steps: {steps}')
