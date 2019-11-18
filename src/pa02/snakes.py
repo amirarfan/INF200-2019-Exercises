ccaf820ffb6ee6912bb0434146157e44b97aa2ff
Sebastian Becker <sebabeck@nmbu.no>
2019-11-18 08:29:22 +0100

    Board class

--- src/pa02/snakes.py
+++ src/pa02/snakes.py
@@ -3,3 +3,10 @@
 __author__ = 'Amir Arfan, Sebastian Tobias Becker'
 __email__ = 'amar@nmbu.no, sebabeck@nmbu.no'
 
+class Board:
+    def __init__(self, ladders_and_snakes, goal):
+        pass
+    def goal_reached(self):
+        pass
+    def position_adjustment(self):
+        pass
