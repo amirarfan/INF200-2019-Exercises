6b0119554f8038fd8b7a9d53bda2365ddb878a92
Amir Arfan <amirarfan7@gmail.com>
2019-11-21 15:04:30 +0100

    Finished creating tests, and adjusted chutes_simulation

--- src/pa02/chutes_simulation.py
+++ src/pa02/chutes_simulation.py
@@ -7,6 +7,9 @@ import random
 
 
 class Board:
+    """
+    Board Class, creates a board
+    """
     standard_board = {
         1: 40,
         8: 10,
@@ -27,6 +30,11 @@ class Board:
     standard_goal = 90
 
     def __init__(self, snakes_and_ladders=None, goal=None):
+        """
+
+        :param snakes_and_ladders: Dictionary board
+        :param goal: Int value if goal is reached player wins.
+        """
 
         if snakes_and_ladders is None:
             self.board = Board.standard_board
@@ -37,9 +45,19 @@ class Board:
         self.change_position = 0
 
     def goal_reached(self, cur_pos):
+        """
+
+        :param cur_pos: Takes current position as input, integer value.
+        :return: Returns True or False whether player has reached goal
+        """
         return cur_pos >= self.goal
 
     def position_adjustment(self, cur_pos):
+        """
+
+        :param cur_pos: Takes in the current position as input, integer
+        :return: Returns the new position after checking the dictionary(board)
+        """
         if cur_pos in self.board.keys():
             self.change_position = self.board[cur_pos]
         else:
@@ -49,15 +67,24 @@ class Board:
 
 
 class Player:
-    def __init__(self, board=None):
-        if board is None:
-            self.board = Board()
-        else:
-            self.board = board
+    """
+    Player Class
+    """
+    def __init__(self, board):
+        """
+        :param board: takes in board as input, determines which board the
+        player shall play on
+        """
+        self.board = board
         self.num_moves = 0
         self.position = 0
 
     def move(self):
+        """
+        Moves the player , by throwing a dice done with random function.
+        Updates the position and increases the number of moves.
+
+        """
         self.position += random.randint(1, 6)
 
         if self.board.position_adjustment(self.position) == 0:
@@ -68,12 +95,28 @@ class Player:
 
 
 class ResilientPlayer(Player):
+    """
+    Resilient Player class, this player takes one extra step the next round
+    if he fell down in the previous round.
+    """
     def __init__(self, board, extra_steps=1):
+        """
+
+        :param board: Takes in board as input
+        :param extra_steps: Takes in amount of steps he should take as input
+        """
         super().__init__(board)
         self.extra_steps = extra_steps
         self.went_down = False
 
     def move(self):
+        """
+        Moves the ResilientPlayer, done with if tests.
+        Updates the current position and increases the number of moves taken.
+        Simple bool if test to check whether the player should take an extra
+        step.
+
+        """
         if self.went_down:
             self.position += random.randint(1, 6) + self.extra_steps
         else:
@@ -93,18 +136,33 @@ class ResilientPlayer(Player):
 
 
 class LazyPlayer(Player):
+    """
+    LazyPlayer class, this player takes one step backwards next round
+     if he climbed in the previous round.
+    """
     def __init__(self, board, dropped_steps=1):
+        """
+
+        :param board: Takes board as input
+        :param dropped_steps:  Takes amount fo steps he should drop next round
+        """
         super().__init__(board)
         self.drop_steps = dropped_steps
         self.went_up = False
 
     def move(self):
+        """
+        Move function, moves player. Basically the same as ResilientPlayer,
+        however this one does a simple bool test to check wheter the player
+        should drop steps.
+
+        """
         if self.went_up:
             self.position += random.randint(1, 6) + self.drop_steps
         else:
             self.position += random.randint(1, 6)
 
-        if self.position > self.board.position_adjustment(self.position):
+        if self.position < self.board.position_adjustment(self.position):
             self.went_up = True
         else:
             self.went_up = False
@@ -118,9 +176,21 @@ class LazyPlayer(Player):
 
 
 class Simulation:
+    """
+    Simulation class to simulate games using the different Player classes.
+    """
     def __init__(
         self, player_field, board=None, seed=69, randomize_players=False
     ):
+        """
+
+        :param player_field: This is a list containing classes of the different
+        players.
+        :param board: The board the players should play on
+        :param seed: Seed for the random module
+        :param randomize_players: Whether or not the order of players should be
+        shuffleed.
+        """
         if board is None:
             self.board = Board()
         else:
@@ -138,6 +208,10 @@ class Simulation:
         ]
 
     def single_game(self):
+        """
+        Simulates a single game
+
+        """
 
         if self.randomtf:
             random.shuffle(self.player_classes)
@@ -150,13 +224,25 @@ class Simulation:
                     return player.num_moves, type(player).__name__
 
     def run_simulation(self, number_of_simulation):
+        """
+        Simulates the amount of games which is given as input.
+
+        """
         for simulation in range(number_of_simulation):
             self.res_simulation.append(self.single_game())
 
     def get_results(self):
+        """
+
+        :return: Returns the result list containing winners.
+        """
         return self.res_simulation
 
     def winners_per_type(self):
+        """
+
+        :return: Returns the winners per type, using zip and dict comprehension
+        """
         winners = list(zip(*self.res_simulation))
 
         winner_dict = {
@@ -167,6 +253,11 @@ class Simulation:
         return winner_dict
 
     def durations_per_type(self):
+        """
+
+        :return: Returns the duration per player type, using dict comprehension
+        and a list comprehension combined with a for loop.
+        """
         duration_dict = {
             type_player: 0 for type_player in self.types_of_players
         }
@@ -180,6 +271,10 @@ class Simulation:
         return duration_dict
 
     def players_per_type(self):
+        """
+
+        :return: Returns player  per type. Counting through the player classes
+        """
         players_dict = {players.__name__: 0 for players in self.player_classes}
         for players in self.player_classes:
             players_dict[players.__name__] += 1
@@ -193,3 +288,4 @@ if __name__ == "__main__":
     print(sim.get_results())
     print(sim.winners_per_type())
     print(sim.durations_per_type())
+    print(sim.players_per_type())
--- src/pa02/test_pa02.py
+++ src/pa02/test_pa02.py
@@ -4,11 +4,14 @@ __author__ = "Amir Arfan, Sebastian Tobias Becker"
 __email__ = "amar@nmbu.no, sebabeck@nmbu.no"
 
 import chutes_simulation as sa
-import pytest
+import pytest  # Pep8 tells me to remove this, but crucial for the testing
 
 
 def test_move():
-    player = sa.Player()
+    """
+    Tests whether the player has moved a position
+    """
+    player = sa.Player(sa.Board())
     first_pos = player.position
     player.move()
     second_pos = player.position
@@ -16,14 +19,70 @@ def test_move():
 
 
 def test_winner_get_result():
+    """
+    Tests that the winner is correct
+    """
     sim = sa.Simulation([sa.LazyPlayer])
     sim.run_simulation(1)
     assert (sim.get_results()[0][1]) == sa.LazyPlayer.__name__
 
 
 def test_amount_of_winners_single_game():
+    """
+    Test that there is only one winner per game
+    """
     sim = sa.Simulation([sa.LazyPlayer, sa.Player, sa.ResilientPlayer])
     sim.run_simulation(1)
-    assert not (len(sim.get_results()) == 2)  # Tests that there is only one
-    # winner per game
-    assert (len(sim.get_results()) == 1)
+    assert not (len(sim.get_results()) == 2)  # Assert not to check that the
+    # value is not higher than one.
+    assert len(sim.get_results()) == 1
+
+
+def test_steps_taken():
+    """
+    Test to assure that no one can win without taking any steps
+
+    """
+    sim = sa.Simulation([sa.LazyPlayer, sa.Player, sa.ResilientPlayer])
+    sim.run_simulation(1)
+    assert sim.get_results()[0][0] > 0
+
+
+def test_shuffle():
+    """
+     Tests if player list actually gets shuffled if told so
+    """
+    player_list = [sa.LazyPlayer, sa.Player, sa.ResilientPlayer]
+    sim = sa.Simulation(
+        [sa.LazyPlayer, sa.Player, sa.ResilientPlayer], randomize_players=True
+    )
+    sim.single_game()
+    assert player_list != sim.player_classes
+
+
+def test_player_per_type():
+    """
+    Tests if amount of player per type gives correct output
+     """
+    player_list = [sa.LazyPlayer, sa.LazyPlayer, sa.Player, sa.ResilientPlayer]
+    sim = sa.Simulation(player_list)
+    sim.run_simulation(1)
+    assert sim.players_per_type()["LazyPlayer"] == 2
+    assert sim.players_per_type()["Player"] == 1
+    assert not sim.players_per_type()["ResilientPlayer"] == 2
+
+
+def test_resilient_player_wins():
+    """
+    From how Resilient player is coded, he should win given a higher amount
+    of simulations and the same seed
+    """
+
+    player_list = [sa.LazyPlayer, sa.Player, sa.ResilientPlayer]
+    sim = sa.Simulation(player_list)
+    sim.run_simulation(1)
+    winner_types = sim.winners_per_type()
+    assert (
+        winner_types["ResilientPlayer"] > winner_types["LazyPlayer"]
+        and winner_types["ResilientPlayer"] > winner_types["Player"]
+    )
